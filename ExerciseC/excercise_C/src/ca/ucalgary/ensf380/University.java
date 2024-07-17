1a3c8641e45f2c4fb27549daa48f58c8869332cf
Shivansh Sharma <shivansh.sharma@ucalgary.ca>
2024-07-17 14:39:10 -0600

    changes to univesity.java

--- ExerciseC/excercise_C/src/ca/ucalgary/ensf380/University.java
+++ ExerciseC/excercise_C/src/ca/ucalgary/ensf380/University.java
@@ -8,8 +8,8 @@ public class University {
         Address addrStudentB = new Address("789 Oak St", "Calgary", "AB", "T2N 1N4", "Canada");
 
         Professor profSmith = new Professor("Dr. Smith", "403-123-4567", "smith@ucalgary.ca", addrProf, 90000.0);
-        Student studentA = new Student("Alice", "403-987-6543", "alice@student.ucalgary.ca", addrStudentA, profSmith, 85.0);
-        Student studentB = new Student("Bob", "403-654-3210", "bob@student.ucalgary.ca", addrStudentB, profSmith, 70.0);
+        Student studentA = new Student("Shivansh Sharma", "403-987-6543", "shivansh@.ucalgary.ca", addrStudentA, profSmith, 85.0);
+        Student studentB = new Student("Mohit Narula", "403-654-3210", "mohit@.ucalgary.ca", addrStudentB, profSmith, 70.0);
 
         System.out.println(profSmith);
         System.out.println(studentA);
